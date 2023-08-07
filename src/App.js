e7771bfd3b4cfcd3f5624fce992e711fcc3a3cab
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-07 16:17:34 +0530

    route handlers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Register from './Pages/Register'
 import './index.css'
 import Login from './Pages/Login'
+import Home from './Pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <>
-    <Register/>
-    {/* <Login/> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter >
   )
 }
