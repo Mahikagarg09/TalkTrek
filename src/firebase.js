8271265e582b7ce8caac96013e740cf778027776
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-07 16:50:33 +0530

    keys hidden

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAE-NgteqqcqIE-j7kpL10jljhfm45mYN8",
-  authDomain: "talktrek-a976c.firebaseapp.com",
-  projectId: "talktrek-a976c",
-  storageBucket: "talktrek-a976c.appspot.com",
-  messagingSenderId: "559410214981",
-  appId: "1:559410214981:web:1e7fc89fd556fd74f70b2c",
-  measurementId: "G-REVGQS4BMW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -19,6 +17,4 @@ export const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-
 export const provider = new GoogleAuthProvider();
-
