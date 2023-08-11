a950f4ee4d0222685b255da2e701d2d48f8d76c2
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-11 15:32:53 +0530

    add image option

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,51 +1,30 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword,signInWithPopup } from "firebase/auth";
-import { auth,provider} from "../firebase"
-import {useNavigate,Link} from "react-router-dom";
+import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth} from "../firebase"
+import { useNavigate, Link } from "react-router-dom";
+import uploadimage from "../assets/upload-image.jpg"
 
-export default function Register({setIsAuth}) {
+export default function Register({ setIsAuth }) {
 
-    const[err,setErr]=useState(false);
-    const navigate= useNavigate();
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate();
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const name = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
 
-        try{
+        try {
             await createUserWithEmailAndPassword(auth, email, password);
             setIsAuth(true);
             navigate("/login");
-            
-        }catch(err){
+
+        } catch (err) {
             setErr(true);
         }
 
     }
-
-    const handleGoogleSignIn = async () => {
-       
-        try {
-            const userCredential = await signInWithPopup(auth, provider);
-
-            setIsAuth(true);
-
-            const user = {
-                uid: userCredential.user.uid,
-                email: userCredential.user.email,
-                isAuth: true,
-            };
-            localStorage.setItem('user', JSON.stringify(user));
-            navigate("/chat");
-
-        } catch (err) {
-          setErr(true);
-            
-        }
-      };
-
     return (
         <section class="bg-gray-50 dark:bg-gray-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -63,7 +42,7 @@ export default function Register({setIsAuth}) {
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Create and account
+                            Create an account
                         </h1>
                         <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                             <div>
@@ -71,7 +50,7 @@ export default function Register({setIsAuth}) {
                                     for="name"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
-                                    Full Name
+                                    Display Name
                                 </label>
                                 <input
                                     type="text"
@@ -114,6 +93,20 @@ export default function Register({setIsAuth}) {
                                     required=""
                                 />
                             </div>
+                            <button class="flex items-center justify-center w-full text-white focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-cente bg-teal-500">
+                                <label htmlFor="upload-avatar" class="flex items-center cursor-pointer">
+                                    <img class="w-7 h-7 mr-2" src={uploadimage} alt="Upload Avatar" />
+                                    Add an avatar
+                                </label>
+                                <input
+                                    id="upload-avatar"
+                                    type="file"
+                                    accept="image/*"
+                                    className="hidden"
+                                    // onChange={(e) => handleImageUpload(e)}
+                                />
+                            </button>
+
                             <button
                                 type="submit"
                                 class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -132,21 +125,13 @@ export default function Register({setIsAuth}) {
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account?{" "}
                                 <Link
-                                    to ="/login"
+                                    to="/login"
                                     class="font-medium text-blue-600 hover:underline dark:text-blue-500"
                                 >
                                     Login here
                                 </Link>
                             </p>
                         </form>
-                        <button
-                                type="submit"
-                                class="w-full text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-350 mr-2 mb-2"
-                                onClick={handleGoogleSignIn}
-                            >
-                                <svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
-                                <span class="flex-grow">Sign in with Google</span>
-                            </button>
                     </div>
                 </div>
             </div>
