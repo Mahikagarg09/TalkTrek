3c645f858d77bd4d5879ca9d3a7ae9d288486e3f
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-11 14:00:37 +0530

    layout of chat page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Leftside from '../components/Leftside'
+import RightSide from '../components/RightSide'
 
-export default function Home({setIsAuth}) {
+export default function Home({ setIsAuth }) {
   return (
-    <div>Home</div>
+    <div class=" flex">
+      <nav class="w-1/3 flex-none ...">
+        <Leftside/>
+      </nav>
+      <main class="flex-1 min-w-0 overflow-auto ...">
+      <RightSide/>
+      </main>
+    </div>
+
+
   )
 }
