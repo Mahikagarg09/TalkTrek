69c1bdfe5df9968bfad58a5cc0a5b262183509c7
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-12 20:58:36 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import {React,useState,useEffect} from 'react'
-import Register from './Pages/Register'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Register  from './Pages/Register';
+import Home from './Pages/Home';
 import './index.css'
-import Login from './Pages/Login'
-import Home from './Pages/Home'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function App() {
-  const[isAuth,setIsAuth]=useState(false);
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.isAuth) {
-      setIsAuth(true);
-    }
-  }, []);
-
   return (
-    <>
-      {isAuth ? (
-        <Router>
-          <Routes>
-            <Route path="/chat" element={<Home setIsAuth={setIsAuth} />} />
-          </Routes>
-        </Router>
-      ) : (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Register setIsAuth={setIsAuth} />} />
-            <Route path="login" element={<Login setIsAuth={setIsAuth} />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <Home />
+            }
+          />
+          <Route path="login" element={<Login/>} />
+          <Route path="register" element={<Register />} />
+        </Route>
       </Routes>
-    </Router>
-      )}
-    </>
+    </BrowserRouter>
   )
 }
