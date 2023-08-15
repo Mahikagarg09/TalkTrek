5dfd717015dfb059c7f00a8c2448abeb288a3c2f
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-15 13:11:14 +0530

    class to className

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,11 @@ import RightSide from '../components/RightSide'
 
 export default function Home({ setIsAuth }) {
   return (
-    <div class=" flex">
-      <nav class="w-1/3 flex-none ...">
+    <div className=" flex">
+      <nav className="w-1/3 flex-none ...">
         <Leftside/>
       </nav>
-      <main class="flex-1 min-w-0 overflow-auto ...">
+      <main className="flex-1 min-w-0 overflow-auto ...">
       <RightSide/>
       </main>
     </div>
