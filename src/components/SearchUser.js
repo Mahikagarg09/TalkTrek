99c4d9373c7fd2edaa6be1727014c8805e98258a
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-15 14:41:17 +0530

    search user component

---
+++ src/components/SearchUser.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import emptyprofile from '../assets/emptyprofile.jpg';
+
+export default function SearchUser({ user }) {
+
+    return (
+        
+        <div>
+            <div
+                className="h-14 w-full mt-2 flex items-center justify-between hover:bg-gray-200 hover:bg-opacity-60 rounded-md px-2 py-1 cursor-pointer transition duration-200"
+            >
+                <div className="h-5/6 flex items-center space-x-2">
+                    <img
+                        src={user.photoURL ||  emptyprofile}
+                        alt="profilePicture"
+                        className="w-10 h-10 rounded-full object-cover"
+                    />
+                    <div>
+                        <p className="font-bold text-gray-800">
+                            {user.name}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
