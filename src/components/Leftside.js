74b618ba8024fcb107e275ccc49e43ef333e40fd
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-15 14:41:32 +0530

    users on left side of window

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import emptyprofile from '../assets/emptyprofile.jpg'
 import MessageView from './MessageView'
+import SearchUser from './SearchUser'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import { AuthContext } from '../Context/AuthContext';
@@ -45,104 +46,104 @@ export default function Leftside() {
         e.code == 'Enter' && handleSearch();
     }
 
-    const handleSelect = async () =>
-    {
-       //check wether the group(chats in firestore) exists, if not create 
-         const combinedId  = 
-         currentUser.uid > user.uid 
-         ? currentUser.uid + user.uid 
-          : user.uid + currentUser.uid;
-      
-          
-          
-         try{
-          //if the chat between the user is exist 
-           //we store in the response constant
-      
-             
-           const res = await getDoc(doc(db,"chats",combinedId));
-           console.log(res)
-          /*If the Response that is chat between the user is not exsit we create the Chats  */
-           if(!res.exists()){
-             
-              //creating chat in chats collection 
-               console.log(res)
-              
-              await setDoc(doc(db,"chats",combinedId), {messages :[] });
-              //create user Chats
-  
-              /* while Creating user Chats in search bar what we need is 
-                 *both userID(id)
-                 *last message {we want to show at bottom of every user}
-                 *displayName (dn)
-                 *img 
-                 *date {we are going to sort the date in this file }
-              */
-               
-           /*   usersChat:{
-                 joyeId :{              
-                   combinedId :{
-                         userInfo{
-                            dn,img,id 
-                         },
-                         lastMessage:""       //rightnow we dont have any chat 
-                         date:               //it is current date
-                   }
-                 }
-              } */ 
-              
-              await updateDoc(doc(db,"userchats",currentUser.uid),{
-                   [combinedId + ".userInfo"] :{
-                      
-                      uid : user.uid,
-                      displayName : user.name,
-                      photoURL : user.photoURL,
-  
-                   },
-  
-                   [combinedId + ".date"] : serverTimestamp() ,   //serverTimeStamp is  function of firebase it calculated time according  to different time zone 
-  
-                  /* RightNow we are not intialising the bottom msg of the user
-                     Because right now we dont have any current user 
-  
-                     we are updating it when we Send msg to friend
-                  */  
-  
-              });
-  
-              /* We are doing same thing for the other side of  user  */
-               
-              await updateDoc(doc(db,"userchats",user.uid),{
-                [combinedId + ".userInfo"] :{
-                   
-                   uid : currentUser.uid,
-                   displayName : currentUser.displayName,
-                   photoURL : currentUser.photoURL
-  
-                },
-  
-                [combinedId + ".date"] : serverTimestamp() ,   //serverTimeStamp is  function of firebase it calculated time according  to different time zone 
-  
-               /* RightNow we are not intialising the bottom msg of the user
-                  Because right now we dont have any current user 
-  
-                  we are updating it when we Send msg to friend
-               */  
-  
-           });
-  
-  
-           }
-         }
-         catch(err){}
-        
-      setUser(null);
-      setsearchValue("");
-         
-       //create user chats
+    //--------------------------------IF ANY USER IS SELECTED TO CHAT----------------
+
+    const handleSelect = async () => {
+        //check wether the group(chats in firestore) exists, if not create 
+        const combinedId =
+            currentUser.uid > user.uid
+                ? currentUser.uid + user.uid
+                : user.uid + currentUser.uid;
+
+
+
+        try {
+            //if the chat between the user is exist 
+            //we store in the response constant
+
+
+            const res = await getDoc(doc(db, "chats", combinedId));
+            console.log(res)
+            /*If the Response that is chat between the user is not exsit we create the Chats  */
+            if (!res.exists()) {
+
+                //creating chat in chats collection 
+                console.log(res)
+
+                await setDoc(doc(db, "chats", combinedId), { messages: [] });
+                //create user Chats
+
+                /* while Creating user Chats in search bar what we need is 
+                   *both userID(id)
+                   *last message {we want to show at bottom of every user}
+                   *displayName (dn)
+                   *img 
+                   *date {we are going to sort the date in this file }
+                */
+
+                /*   usersChat:{
+                      joyeId :{              
+                        combinedId :{
+                              userInfo{
+                                 dn,img,id 
+                              },
+                              lastMessage:""       //rightnow we dont have any chat 
+                              date:               //it is current date
+                        }
+                      }
+                   } */
+
+                await updateDoc(doc(db, "userchats", currentUser.uid), {
+                    [combinedId + ".userInfo"]: {
+
+                        uid: user.uid,
+                        displayName: user.name,
+                        photoURL: user.photoURL,
+
+                    },
+
+                    [combinedId + ".date"]: serverTimestamp(),   //serverTimeStamp is  function of firebase it calculated time according  to different time zone 
+
+                    /* RightNow we are not intialising the bottom msg of the user
+                       Because right now we dont have any current user 
+    
+                       we are updating it when we Send msg to friend
+                    */
+
+                });
+
+                /* We are doing same thing for the other side of  user  */
+
+                await updateDoc(doc(db, "userchats", user.uid), {
+                    [combinedId + ".userInfo"]: {
+
+                        uid: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        photoURL: currentUser.photoURL
+
+                    },
+
+                    [combinedId + ".date"]: serverTimestamp(),   //serverTimeStamp is  function of firebase it calculated time according  to different time zone 
+
+                    /* RightNow we are not intialising the bottom msg of the user
+                       Because right now we dont have any current user 
+       
+                       we are updating it when we Send msg to friend
+                    */
+
+                });
+
+
+            }
+        }
+        catch (err) { }
+
+        setUser(null);
+        setsearchValue("");
+
+        //create user chats
     };
 
-    
     return (
         <>
             <div className="sticky bg-white top-0">
@@ -193,7 +194,7 @@ export default function Leftside() {
                 {user &&
                     <div className='hover:bg-gray-200' onClick={handleSelect}>
                         <div className="overflow-auto scrollbar-none mt-2">
-                            <MessageView user={user} />
+                            <SearchUser user={user} />
                         </div>
                         <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300"></div>
                     </div>
--- src/components/MessageView.js
+++ src/components/MessageView.js
@@ -1,55 +1,56 @@
-import React, { useDeferredValue } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import emptyprofile from '../assets/emptyprofile.jpg';
+import { AuthContext } from '../Context/AuthContext';
+import {db} from "../firebase";
+import {doc,onSnapshot} from "firebase/firestore"
 
-export default function MessageView({user}) {
-    // const staticData = [
-    //     {
-    //         id: 1,
-    //         name: 'John Doe',
-    //         lastUpdate: '10:30 AM',
-    //         lastMessage: 'Hello there!',
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'Jane Smith',
-    //         lastUpdate: 'Yesterday',
-    //         lastMessage: 'How are you?',
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'Alice Johnson',
-    //         lastUpdate: '2 days ago',
-    //         lastMessage: 'See you soon!',
-    //     },
-    // ];
+export default function MessageView() {
+    const { currentUser } = useContext(AuthContext);
+
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userchats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+
+            return () => {
+                unsub();
+            };
+        };
+        currentUser.uid && getChats();
+    }, [currentUser.uid]);
 
     return (
-        <div>
-            {/* {staticData.map(user => ( */}
-                <div
-                    className="h-14 w-full mt-2 flex items-center justify-between hover:bg-gray-200 hover:bg-opacity-60 rounded-md px-2 py-1 cursor-pointer transition duration-200"
-                >
-                    <div className="h-5/6 flex items-center space-x-2">
-                        <img
-                            src={user && user.photoURL ? user.photoURL : emptyprofile}
-                            alt="profilePicture"
-                            className="w-10 h-10 rounded-full object-cover"
-                        />
-                        <div>
-                            <p className="font-bold text-gray-800">
-                                {user && user.name}
-                            </p>
-                            <p className="text-green text-sm">
-                                Last Message: Hi
-                            </p>
+        <>
+            {Object.entries(chats)?.map((chat) => (
+                <div key={chat[0]}>
+                    <div
+                        className="h-14 w-full mt-2 flex items-center justify-between hover:bg-gray-200 hover:bg-opacity-60 rounded-md px-2 py-1 cursor-pointer transition duration-200"
+                    >
+                        <div className="h-5/6 flex items-center space-x-2">
+                            <img
+                                src={chat[1].userInfo.photoURL || emptyprofile}
+                                alt="profilePicture"
+                                className="w-10 h-10 rounded-full object-cover"
+                            />
+                            <div>
+                                <p className="font-bold text-gray-800">
+                                    {chat[1].userInfo.displayName}
+                                </p>
+                                <p className="text-green text-sm">
+                                    {chat[1].lastMessage?.text}
+                                </p>
+                            </div>
                         </div>
+                        <p className="text-xs font-medium text-gray-800">
+                            {/* You can add the formatted date/time here */}
+                            10.am
+                        </p>
                     </div>
-                    <p className="text-xs font-medium text-gray-800">
-                        {/* {person.lastUpdate} */}
-                        10:00 AM
-                    </p>
                 </div>
-             {/* ))} */}
-        </div>
+            ))}
+        </>
     );
 }
