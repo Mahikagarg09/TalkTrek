66d5be158fc86be9f9f0123452d4cd6470a47f2a
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-23 19:03:30 +0530

    emoji bug fix

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -1,19 +1,19 @@
-import {React,useState,useRef,useEffect, useContext} from 'react'
-import EmojiPicker, { IEmojiData } from "emoji-picker-react";
+import { React, useState, useRef, useEffect, useContext } from 'react'
+import EmojiPicker from "emoji-picker-react";
 import { AuthContext } from '../Context/AuthContext';
 import { ChatContext } from '../Context/ChatContext';
-import { Timestamp, arrayUnion, doc,  updateDoc ,serverTimestamp} from 'firebase/firestore';
+import { Timestamp, arrayUnion, doc, updateDoc, serverTimestamp } from 'firebase/firestore';
 import { db, storage } from '../firebase';
 import { v4 as uuid } from 'uuid';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 export default function MessageSend() {
-    const[text,setText]= useState("");
-    const[img,setImg]=useState(null);
+    const [text, setText] = useState("");
+    const [img, setImg] = useState(null);
     const [openPicker, setOpenPicker] = useState(false);
     const pickerRef = useRef(null);
-    const {currentUser} = useContext(AuthContext);
-    const {data} = useContext(ChatContext);
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
 
 
     useEffect(() => {
@@ -23,77 +23,77 @@ export default function MessageSend() {
             }
         };
 
-        document.addEventListener("click", handleClickOutside);
+        document.addEventListener('click', handleClickOutside);
 
         return () => {
-            document.removeEventListener("click", handleClickOutside);
+            document.removeEventListener('click', handleClickOutside);
         };
     }, []);
 
-    const onEmojiClick = (_, emojiObject) => {
-        setText(text + emojiObject.emoji);
-    };
+    const onEmojiClick = (event) => {
+        setText((prevtext) => prevtext +event.emoji);
+    }
 
-    const handleSend = async() =>{
-        // if(img){
-        //     const storageRef =ref(storage,uuid());
-        //     const uploadTask = uploadBytesResumable(storageRef,img);
+    const handleSend = async () => {
+        if (img) {
+            const storageRef = ref(storage, uuid());
+            const uploadTask = uploadBytesResumable(storageRef, img);
 
-        //     uploadTask.on(
-        //         (error) =>{
+            uploadTask.on(
+                (error) => {
 
-        //         },
-        //         () =>{
-        //             getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-        //                 messages:arrayUnion ({
-        //                     id:uuid(),
-        //                     text,
-        //                     senderId:currentUser.uid,
-        //                     date:Timestamp.now(),
-        //                     img:downloadURL
-        //                 })
-        //             })
-        //         }
-        //     )
-        // }else{
-            await updateDoc(doc(db,"chats" ,data.chatId) , {
-                messages : arrayUnion({
-                    id:uuid(),
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        messages: arrayUnion({
+                            id: uuid(),
+                            text,
+                            senderId: currentUser.uid,
+                            date: Timestamp.now(),
+                            img: downloadURL
+                        })
+                    })
+                }
+            )
+        } else {
+            await updateDoc(doc(db, "chats", data.chatId), {
+                messages: arrayUnion({
+                    id: uuid(),
                     text,
-                    senderId:currentUser.uid,
-                    date:Timestamp.now()
+                    senderId: currentUser.uid,
+                    date: Timestamp.now()
                 })
             })
+        }
 
-            await updateDoc(doc(db,"userchats",currentUser.uid) , {
-                [data.chatId + ".lastMessage"]:{
-                    text
-                },
-                [data.chatId + ".date"]:
-                    serverTimestamp(),
-                
-            })
+        await updateDoc(doc(db, "userchats", currentUser.uid), {
+            [data.chatId + ".lastMessage"]: {
+                text
+            },
+            [data.chatId + ".date"]:
+                serverTimestamp(),
 
-            await updateDoc(doc(db,"userchats",data.user.uid) , {
-                [data.chatId + ".lastMessage"]:{
-                    text
-                },
-                [data.chatId + ".date"]:
-                    serverTimestamp()
-                
-            })
-            setImg(null);
-            setText("");
-        // }
+        })
+
+        await updateDoc(doc(db, "userchats", data.user.uid), {
+            [data.chatId + ".lastMessage"]: {
+                text
+            },
+            [data.chatId + ".date"]:
+                serverTimestamp()
+
+        })
+        setImg(null);
+        setText("");
     }
 
     return (
         <div
             className="px-4 shadow-md py-2 w-full bg-gray-50 flex border-2 border-green rounded-xl"
         >
-            {/* <i class="fa-sharp fa-regular fa-paperclip" style="color: #094c4e;"></i>
-            <input type="file" style={{display : "none"}} id="file" onChange={e => setImg(e.target.files[0])} />
-            <label htmlFor='file'><img src={} /></label> */}
+            <input type="file" style={{ display: "none" }} id="file" onChange={e => setImg(e.target.files[0])} />
+            <label htmlFor='file'> <i className="fa-sharp fa-regular fa-paperclip" style={{ color: "#094c4e" }}></i>
+            </label>
             <input
                 className="outline-none w-full px-4 py-2 bg-transparent font-medium"
                 placeholder="Enter a Message"
@@ -104,7 +104,7 @@ export default function MessageSend() {
                 <div className="relative">
                     {openPicker && (
                         <div className="absolute bottom-0 -right-10">
-                            <EmojiPicker preload onEmojiClick={console.log("emoji clicked")} />
+                            <EmojiPicker onEmojiClick={onEmojiClick} />
                         </div>
                     )}
                     <button
