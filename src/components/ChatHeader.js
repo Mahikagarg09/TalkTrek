b84594b4b81a657306a8122518b49105fe2c357e
Mahikagarg09 <mahika_garg@yahoo.com>
2023-09-07 16:39:33 +0530

    delete chat icon

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -3,34 +3,25 @@ import emptyProfile from "../assets/emptyprofile.jpg"
 import { ChatContext } from '../Context/ChatContext';
 
 export default function ChatHeader() {
-    const {data} = useContext(ChatContext);
+    const { data } = useContext(ChatContext);
     return (
-        <div className="flex space-x-2 px-2 py-7 shadow-md items-center dark:bg-cyan-900 w-full h-10 top-0 	position: fixed">
-            <div
-                className="cursor-pointer mr-5 md:hidden"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                    />
-                </svg>
+        <div class="flex sm:items-center justify-between py-2  bg-cyan-900">
+            <div class="relative flex items-center space-x-4">
+                <div class="relative ml-5">
+                    <img src={data.user?.photoURL || emptyProfile} alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full" />
+                </div>
+                <div class="flex flex-col leading-tight">
+                    <div class="text-2xl mt-1 flex items-center">
+                        <span class="text-white mr-3">{data.user?.displayName}</span>
+                    </div>
+                </div>
             </div>
-            <div className="flex items-center">
-                <img
-                    src={data.user?.photoURL || emptyProfile}
-                    className="h-10 w-10 rounded-full pointer-events-none object-cover"
-                    alt="whatsapp"
-                />
-            </div>
-            <div>
-                <p className="text-white font-semibold">{data.user?.displayName}</p>
+            <div class="flex items-center space-x-2 mr-7">
+                <button type="button" class="inline-flex items-center justify-center rounded-lg  h-8 w-8 transition duration-500 ease-in-out focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+                        <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{ "mix-blend-mode": "normal" }}><g transform="scale(8.53333,8.53333)"><path d="M14.98438,2.48633c-0.55152,0.00862 -0.99193,0.46214 -0.98437,1.01367v0.5h-5.5c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-1.48633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-1.48633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805h-5.5v-0.5c0.0037,-0.2703 -0.10218,-0.53059 -0.29351,-0.72155c-0.19133,-0.19097 -0.45182,-0.29634 -0.72212,-0.29212zM6,9l1.79297,15.23438c0.118,1.007 0.97037,1.76563 1.98438,1.76563h10.44531c1.014,0 1.86538,-0.75862 1.98438,-1.76562l1.79297,-15.23437z"></path></g></g>
+                    </svg>
+                </button>
             </div>
         </div>
     )
